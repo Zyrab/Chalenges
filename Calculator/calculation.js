056388fde68b0d744ba8b3425ce46ed2e22bd2ca
zyrab <zurakrua@gmail.com>
2023-06-11 17:40:45 +0200

    rekonstruction

---
+++ Calculator/calculation.js
@@ -0,0 +1,148 @@
+//variables
+const calculator = document.querySelector(".Calculator");
+const historyArr = [];
+var isEntered = false;
+var symbolIsUsed = false;
+//eventlistener
+calculator.addEventListener('click', e =>{ 
+    var big = document.getElementById('result');
+    var small = document.getElementById('input');
+
+    var eId = e.target.id;
+    var eClss = e.target.className;
+    var eText = e.target.innerText
+
+     //calculator screen clear
+     if(eId ==='Ac'){
+        big.innerText=0;
+        small.innerText='';
+        isEntered = false;
+        symbolIsUsed = false;
+    }
+    //back button functionality
+    if((eId ==='back' || eId ==='backSVG')  && isEntered === true){
+        if( big.innerText.length== 1 || big.innerText==='0'){
+            big.innerText =0;
+        }else{
+        big.innerText=big.innerText.slice(0, -1);
+        }
+    }
+    //Dot is printed only once
+    if(eClss ==="numbD" || eClss ==="numbD Whitetheme" ) {
+        if( isEntered === false && big.innerText !==0){
+            big.innerText ='0.';
+            isEntered = true;
+            console.log(big.innerText);
+        }
+        if(big.innerText.includes(".")){ 
+            big.innerText+='';
+        }else{
+            big.innerText +='.';
+            isEntered = true;
+        }
+    }
+
+
+
+
+    // function calculatorButtonsWorkOnClick () {
+
+    // }
+
+
+    calculationButtons(eText, eId, small, big , eClss)
+    console.log(isEntered);
+    //console.log(symbolIsUsed);
+})
+
+// ereasing unwanted  symbols
+function trimIfNeeded ( big){
+    var trimed;
+    if(big.innerText.at(-1)==='.'){
+        trimed = big.innerText.slice(0, -1);
+    }else{
+         trimed = big.innerText;
+    }
+    if(big.innerText.includes(".")){
+        if(big.innerText.split('.')[1] == 0){
+            trimed = big.innerText.slice(0 , -(big.innerText.split('.')[1].length+1));
+        } else{
+            trimed = trimed = big.innerText;
+        }
+    }
+    return trimed;
+}
+
+//calculation buttons 
+function calculationButtons( elementText , Id , small, big, eClss){
+    if (Id === 'minus' || Id === 'plus' || Id === 'division' || Id === 'multiplier'){   
+        var span = document.createElement('span');
+        span.textContent = elementText;
+        span.style.color = '#339DFF';           
+        if (small.innerText.includes(')')==true){
+            small.innerText=big.innerText;
+            small.appendChild(span);
+            isEntered= false
+        }
+        if (isEntered ===false && small.innerText !==''){
+            small.innerText=small.innerText.slice(0, -1);
+            small.appendChild(span);
+            isEntered= false
+        }
+        if( small.innerText.includes('=')){
+            small.innerText=trimIfNeeded(big)
+            small.appendChild(span);
+            isEntered= false
+        }
+        if( small.innerText ===''){
+            small.innerText=trimIfNeeded (big);
+            small.appendChild(span);
+            isEntered = false;
+        }
+        if( isEntered === true && small.innerText.includes('=')==false ){
+            small.innerText=eval(small.innerText + trimIfNeeded (big));
+            small.appendChild(span);
+            big.innerText = eval(small.innerText.slice(0, -1));
+            isEntered = false;
+            // to add in the history
+        }
+    }
+    if(eClss ==="numb" || eClss ==="numb0" || eClss ==="numb Whitetheme" || eClss ==="numb0 Whitetheme") {
+        if(result.innerText === '0' && elementText!=='.' || isEntered===false){
+            result.innerText=""+elementText;
+            isEntered = true;
+        }else{
+            result.innerText+=elementText;
+            isEntered = true;
+        }
+    }
+     // equation
+     if ( Id === 'equal'){
+        var txt = small.innerText+trimIfNeeded(big);
+        if( txt.includes("(")){
+            small.innerText = small.innerText +'='+big.innerText;
+            isEntered = false;
+        } else{
+            big.innerText = eval(txt);
+            small.innerText = txt +'='+big.innerText;
+            isEntered = false;
+            console.log(big.innerText)
+        }
+        // to add in the history
+    }
+    if(Id === 'pe'){
+            small.innerText += "("+trimIfNeeded(big)+'*'+3.14+")"
+            big.innerText=trimIfNeeded(big)*3.14
+            isEntered=false
+    }
+    if(Id === 'sqr'){
+            small.innerText += "("+trimIfNeeded(big)+'*'+trimIfNeeded(big)+")"
+            big.innerText=trimIfNeeded(big)*trimIfNeeded(big)
+            isEntered=false
+    }
+    if(Id === 'sqroot'){
+            small.innerText += "("+trimIfNeeded(big)+'*'+3.14+")"
+            big.innerText=trimIfNeeded(big)*3.14
+            isEntered=false
+    }
+}
--- Calculator/main.js
+++ Calculator/main.js
@@ -1,9 +1,7 @@
 //variables
-const calculator = document.querySelector(".Calculator")
-const historyArr = []
-var isEntered = false
+const calculator1 = document.querySelector(".Calculator")
 //eventlistener
-calculator.addEventListener('click', e =>{
+calculator1.addEventListener('click', e =>{
     //variables
     var eT = e.target
     var eId = e.target.id
@@ -14,70 +12,18 @@ calculator.addEventListener('click', e =>{
     var calculatorId = document.getElementById('calculatorId')
     var calcElements = calculatorId.querySelectorAll('DIV , BUTTON , SVG')
    
-    var calculate = 0
-
-//amking symbols and numbers work
-    //number display properly
-    if(eClss ==="numb" || eClss ==="numb0" || eClss ==="numb Whitetheme" || eClss ==="numb0 Whitetheme") {
-        if(result.innerText === '0' && eValue!=='.' || isEntered===false){
-            result.innerText=""+eValue
-            isEntered = true
-        }else{
-            result.innerText+=eValue
-            isEntered = true
-        }
-    }
-    //restart
-    if(eId ==='Ac'){
-        result.innerText=0
-        input.innerText=''
-        isEntered = false
-    }
-    //back to not erase 0
-    if((eId ==='back' || eId ==='backSVG')  && isEntered === true){
-        if( result.innerText.length== 1 || result.innerText==='0'){
-            result.innerText=0
-        }else{
-        result.innerText=result.innerText.slice(0, -1)
-         }
-    }
-    //dot to not repeat
-    if(eClss ==="numbD" || eClss ==="numbD Whitetheme" ) {
-        if( isEntered === false && result.innerText !==0){
-            result.innerText='0.'
-            isEntered = true
-        }
-        if(result.innerText.includes(".")){ 
-            result.innerText+=''
-        }else{
-            result.innerText+='.'
-            isEntered = true
-        }
-
-    }
     //theme toggle
     if(eId === 'toggle'){
         selectNodesToChangeCSS( );
     }
-    // + - / *
-    if (eId === 'minus' || eId === 'plus' || eId === 'division' || eId === 'multiplier'){
-    calculationButtons(eValue)
-    } 
-    // sqr X2 P %
-    if (eId === 'sqroot' || eId === 'sqr' || eId === 'pe' || eId === 'perc'  ) {
+    // history toggle
+    if(eId === 'history'){
 
     }
-    // equation
-    if ( eId === 'equal'){
-        result.innerText=equation()
-        input.innerText = ''
-        isEntered = false
+    //Functions
 
-    }
-//Functions
-
-//element selector to change css propertys
-function selectNodesToChangeCSS() {
+    //element selector to change css propertys
+    function selectNodesToChangeCSS() {
         for (i = 0 ; i < calcElements.length; i++) {
             node = calcElements.item(i)
             //white buttons
@@ -118,73 +64,10 @@ function selectNodesToChangeCSS() {
             }
         }     
     }
-
-// trim if .0 or .
-function trimIfNeeded (){
-    var trimed
-    if(result.innerText.at(-1)==='.'){
-        trimed = result.innerText.slice(0, -1)
-    }else{
-         trimed = result.innerText
-    }
-    if(result.innerText.includes(".")){
-        if(result.innerText.split('.')[1] == 0){
-            trimed = result.innerText.slice(0 , -(result.innerText.split('.')[1].length+1))
-        } else{
-            trimed = trimed = result.innerText
-        }
-    }
-    return trimed
-}
-    
-//calculation buttons 
-function calculationButtons( elementText){
-    var span = document.createElement('span');
-    span.textContent = elementText;
-    span.style.color = '#339DFF';           
-    if( input.innerText ===''){
-        input.innerText=trimIfNeeded ()
-        input.appendChild(span)
-        isEntered = false
-    }
-    if( isEntered === true){
-        input.innerText+=trimIfNeeded ()
-        input.appendChild(span)
-        isEntered = false
-    }
-    if (isEntered ===false && input.innerText !==''){
-        input.innerText=input.innerText.slice(0, -1)
-        input.appendChild(span)
-    }
-}
-      
+    function historyToggle(){
         
-
-// function for functionButtons
-function functionButtons (elementText){
-    // 1/x sqr  sqrroo
-    if ( elementText === '%') {
-        if (isEntered){}
     }
-}
-// make Equation to work
-function equation() {
-   var result = input.innerText+trimIfNeeded()
-   historyArr.unshift(result + '='+ eval(result))
-   return eval(result)
-}
-//console.log(isEntered)
-console.log(historyArr)
 
-
-// history to display
-if (eId === 'history'){
-    var div = document.createElement('div');
-    for ( i = 0 ; i < historyArr.length; i++) {
-        div.innerHTML+=i
-    }
-    input.appendChild(div)
-}
 })
 
 
