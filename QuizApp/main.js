3fae9e1b73081ae76facdbe492680b1b1d8845d4
zyrab <zurakrua@gmail.com>
2023-06-24 01:12:22 +0200

    toggle

--- QuizApp/main.js
+++ QuizApp/main.js
@@ -1,8 +1,21 @@
+// constants
+    const forEventListener = document.querySelector('body');
+
+
+    forEventListener.addEventListener('click', e => {
+        var eId = e.target.id
+
+        console.log(e.target)
+        if ( eId === 'topicButton' || eId === 'topicContent' || eId === 'topicContentClose'){
+            ToggleClassList('topicContent' , 'displayed')
+        }
+
+    })
 
 
 
 //Toggle classlis function
     function ToggleClassList ( elementId , elementClassName) {
-        var toggle = document.getElementById(elementId)
-        toggle.classlist.toggle(elementClassName)
+        var toggleClass = document.getElementById(elementId)
+        toggleClass.classList.toggle(elementClassName)
     }
