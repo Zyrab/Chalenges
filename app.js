1d5bebf3baf3f58aa68894348c0468d065850c36
zyrab <zurakrua@gmail.com>
2023-07-07 17:06:10 +0200

    animations-styiling

--- app.js
+++ app.js
@@ -7,3 +7,9 @@ window.addEventListener("scroll", function() {
     // Adjust the opacity based on the scroll position
     scrollUpArrow.style.opacity = 0 + (scrollPosition * 0.001);
   });
+
+  document.querySelector('.nav__trigger').addEventListener('click', function(e) {
+    e.preventDefault();
+    var parentElement = this.parentNode;
+    parentElement.classList.toggle('nav--active');
+  });
