bd3a08ab5daf6070e2176e4f2f7208da7d9f3a9d
zyrab <zurakrua@gmail.com>
2023-07-07 21:14:53 +0200

    dark-mode-favicon

--- app.js
+++ app.js
@@ -13,3 +13,17 @@ window.addEventListener("scroll", function() {
     var parentElement = this.parentNode;
     parentElement.classList.toggle('nav--active');
   });
+  document.querySelector('.mode-bar').addEventListener('click', e => {
+    console.log(e.target)
+    if (e.target.className === 'mode-toggle-button'){
+      toggleClass('dark' , 'hidden')
+      toggleClass('light' , 'hidden')
+      toggleClass('body' , 'dark-mode')
+
+  }
+  });
+
+
+  function toggleClass (elemntid, classlist) {
+    document.getElementById(elemntid).classList.toggle(classlist)
+  }
