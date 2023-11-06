f60fbc32eb526b2429babaed53d726cfa3bdac3a
zyrab <zurakrua@gmail.com>
2023-11-06 21:22:55 +0100

    finishing-project

--- ResultSummery/main.js
+++ ResultSummery/main.js
@@ -1,40 +1,73 @@
-const calculate = document.getElementById('button')
 
 const displaySummeryResults = () => {
         // to get content of json file with quiz content
         fetch('data.json')
         .then(response => response.json())
         .then(summeryResult => {
+        
         let totalScore = 0
-        var score = document.getElementById('score')
-        var summeryDiv = document.getElementById('summeryDiv')
+        let score = document.getElementById('score')
+        let summeryDiv = document.getElementById('summeryDiv')
+        summeryDiv.innerHTML=''
         // loops through json file and gets 10 questions randomly. 
-        for ( i = 0 ; i < summeryResult.length ; i++ ){
+        for ( let i = 0 ; i < summeryResult.length ; i++ ){
             totalScore += summeryResult[i].score
-        //   let question = document.createElement('p')
-        //   //this is to get questions from selected topics and avoid reapiting them. 
-        //   if  (selectedTopics.includes(random.topic) && !displayedQuestions.includes(random)) {  
-        //     //here we add to the p element - text , id , class , and if question id matches 101, wich means this is first question it adds display block, other 9 question get display none.
-        //     question.textContent = random.question 
-        //     question.id = '10' + i
-        //     question.classList = 'quizQuestions'
-        //     question.style.display = [`${question.id === '101' ? 'block' : 'none'}`]
-        //     QuestionDiv.appendChild(question)
-        //     // function to randomise answers each time they are displayed.
-        //     randomiseAnswers (random.answers , question.id , random.answers[random.correctAnswer] )
-        //     displayedQuestions.push(random)
-        //   } else {
-        //     // this will decrement i to in case if - if statment didnt match . to try again and display all 10 questions.
-        //     i--
-        //   }
+            let result = summeryResult[i]
+            console.log(summeryResult[i].category , summeryResult[i].score , summeryResult[i].icon)
+
+            let wrapperDiv = document.createElement('div');
+            wrapperDiv.className = 'wrapper';
+          
+            // Create componentsDiv
+            let componentsDiv = document.createElement('div');
+            componentsDiv.className = 'componentsDiv ' + result.category.toLowerCase() + 'Color'; // Add color class dynamically
+          
+            // Create component div
+            let componentDiv = document.createElement('div');
+            componentDiv.className = 'component';
+          
+            // Create and append img element
+            let img = document.createElement('img');
+            img.src = result.icon;
+            img.alt = result.category.toLowerCase();
+            componentDiv.appendChild(img);
+          
+            // Create and append paragraph for category name
+            let categoryName = document.createElement('p');
+            categoryName.textContent = result.category;
+            componentDiv.appendChild(categoryName);
+          
+            // Append component div to componentsDiv
+            componentsDiv.appendChild(componentDiv);
+          
+            // Create progress paragraph
+            let progressPara = document.createElement('p');
+            progressPara.className = 'progress';
+          
+            // Create and append span for score
+            let scoreSpan = document.createElement('span');
+            scoreSpan.textContent = result.score;
+            progressPara.appendChild(scoreSpan);
+          
+            // Append static text node "/ 100"
+            progressPara.appendChild(document.createTextNode(' / 100'));
+          
+            // Append progress paragraph to componentsDiv
+            componentsDiv.appendChild(progressPara);
+          
+            // Append componentsDiv to wrapperDiv
+            wrapperDiv.appendChild(componentsDiv);
+          
+            // Append wrapperDiv to the parentDiv
+            summeryDiv.appendChild(wrapperDiv);
+
         }     
-        score.innerText = Math.round(totalScore/ summeryResult.length)
+        score.textContent = Math.round(totalScore/ summeryResult.length)
       })
       .catch(error => {
         // Handle any errors that occur during the fetch
         console.error('Error fetching JSON:', error);
       });
 }
-calculate.addEventListener('click' , (e) =>{
-    displaySummeryResults()
-} )
+
+document.getElementById('button').addEventListener('click' , displaySummeryResults)
