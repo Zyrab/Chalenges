185a3ad3cd98576142e93a25154ff047fa0290a6
zyrab <zurakrua@gmail.com>
2023-11-06 22:04:20 +0100

    changed-logic-to-only-score-display

--- ResultSummery/main.js
+++ ResultSummery/main.js
@@ -7,60 +7,12 @@ const displaySummeryResults = () => {
         
         let totalScore = 0
         let score = document.getElementById('score')
-        let summeryDiv = document.getElementById('summeryDiv')
-        summeryDiv.innerHTML=''
         // loops through json file and gets 10 questions randomly. 
         for ( let i = 0 ; i < summeryResult.length ; i++ ){
+            let score = summeryResult[i].score
             totalScore += summeryResult[i].score
-            let result = summeryResult[i]
-            console.log(summeryResult[i].category , summeryResult[i].score , summeryResult[i].icon)
-
-            let wrapperDiv = document.createElement('div');
-            wrapperDiv.className = 'wrapper';
-          
-            // Create componentsDiv
-            let componentsDiv = document.createElement('div');
-            componentsDiv.className = 'componentsDiv ' + result.category.toLowerCase() + 'Color'; // Add color class dynamically
-          
-            // Create component div
-            let componentDiv = document.createElement('div');
-            componentDiv.className = 'component';
-          
-            // Create and append img element
-            let img = document.createElement('img');
-            img.src = result.icon;
-            img.alt = result.category.toLowerCase();
-            componentDiv.appendChild(img);
-          
-            // Create and append paragraph for category name
-            let categoryName = document.createElement('p');
-            categoryName.textContent = result.category;
-            componentDiv.appendChild(categoryName);
-          
-            // Append component div to componentsDiv
-            componentsDiv.appendChild(componentDiv);
-          
-            // Create progress paragraph
-            let progressPara = document.createElement('p');
-            progressPara.className = 'progress';
-          
-            // Create and append span for score
-            let scoreSpan = document.createElement('span');
-            scoreSpan.textContent = result.score;
-            progressPara.appendChild(scoreSpan);
-          
-            // Append static text node "/ 100"
-            progressPara.appendChild(document.createTextNode(' / 100'));
-          
-            // Append progress paragraph to componentsDiv
-            componentsDiv.appendChild(progressPara);
-          
-            // Append componentsDiv to wrapperDiv
-            wrapperDiv.appendChild(componentsDiv);
-          
-            // Append wrapperDiv to the parentDiv
-            summeryDiv.appendChild(wrapperDiv);
-
+            let scoreSpanId = summeryResult[i].category.toLowerCase(); // Or you could use another method for ID if necessary
+            document.getElementById(scoreSpanId).textContent=score  
         }     
         score.textContent = Math.round(totalScore/ summeryResult.length)
       })
